d6fe68a5501c9239c28f12fd1bedbfaadf318b53
Dystriker <dystriker@hotmail.fr>
2020-07-09 21:13:14 +0200

    All Working

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PCAP IP'S parser made by Dylan, Antoine & Antonin
 
-# TODO : Graphics
+# 
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import sys
 import re
 import os.path
-import os
 from pcapfile import savefile
 import xlwt
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
 
@@ -166,7 +163,7 @@ def validate_ip(ip_str):
     else:
         return False
 
-def extract_pcap(pcapfile, user_ip_src, user_ip_dest):
+def extract_pcap(pcapfile):
     try:
         open_pcap = open(pcapfile, 'rb')
     except IOError:
@@ -176,53 +173,43 @@ def extract_pcap(pcapfile, user_ip_src, user_ip_dest):
     packets_ip = pcap_file.packets
     compteur = 0
 
-    if user_ip_src != '' and user_ip_dest != '':
-        for i in range(len(packets_ip)):
-            if("from b'" + user_ip_src + "' to b'" + user_ip_dest + "'" in str(pcap_file.packets[i].packet.payload)):
-                compteur += 1
-        if compteur > 0:
-            print('From ' + user_ip_src + ' to ' + user_ip_dest + ' x' + str(compteur) + ' fois')
-        else:
-            print('Aucune donnée trouvée')
+    pair_matches = []
+    matches = []
+    clean_matches = []
+    clean_pairs = re.compile('ipv4 packet from ([^<]* to [^<]*) carrying')
+        
+    for i in range(len(packets_ip)):
+        pair_matches.append(str(pcap_file.packets[i].packet.payload))
+       
+    for i in pair_matches:
+        clean_pair = re.findall(clean_pairs, i)
+        if len(clean_pair) > 0:
+            matches.append(clean_pair[0].replace('b', '').replace("'", ""))
+            tmp = clean_pair[0].replace('to', '').replace('b', '').replace("'", "")
+            clean_matches.append(tmp)
+        
+    results = [[x, matches.count(x)] for x in set(matches)]
+    clean_results = [[x, clean_matches.count(x)] for x in set(clean_matches)]
 
+    for i in results:
+        print(str(i[0]) + ' -> ' + str(i[1]) + ' fois')
+    
+    toGraphics = str(input("Voulez-vous générer des graphiques ? (o/N) "))
+    if toGraphics.lower() == 'o':
+        generate_graph(clean_results)
+        toExport = str(input("Voulez-vous exporter en CSV ? (o/N) "))
+        if toExport.lower() == 'o':
+            export_as_csv(clean_results)
+        else:
+            print("Merci d'avoir utilisé notre PcapParser !")
+            sys.exit(0)
     else:
-        pair_matches = []
-        matches = []
-        clean_matches = []
-        clean_pairs = re.compile('ipv4 packet from ([^<]* to [^<]*) carrying')
-        
-        for i in range(len(packets_ip)):
-            pair_matches.append(str(pcap_file.packets[i].packet.payload))
-        
-        for i in pair_matches:
-            clean_pair = re.findall(clean_pairs, i)
-            if len(clean_pair) > 0:
-                matches.append(clean_pair[0].replace('b', '').replace("'", ""))
-                tmp = clean_pair[0].replace('to', '').replace('b', '').replace("'", "")
-                clean_matches.append(tmp)
-        
-        results = [[x, matches.count(x)] for x in set(matches)]
-        clean_results = [[x, clean_matches.count(x)] for x in set(clean_matches)]
-
-        for i in results:
-            print(str(i[0]) + ' -> ' + str(i[1]) + ' fois')
-        
-        toGraphics = str(input("Voulez-vous générer des graphiques ? (o/N) "))
-        if toGraphics.lower() == 'o':
-            generate_graph(clean_results)
-            toExport = str(input("Voulez-vous exporter en CSV ? (o/N) "))
-            if toExport.lower() == 'o':
-                export_as_csv(clean_results)
-            else:
-                print("Merci d'avoir utilisé notre PcapParser !")
-                sys.exit(0)
+        toExport = str(input("Voulez-vous exporter en CSV ? (o/N) "))
+        if toExport.lower() == 'o':
+            export_as_csv(clean_results)
         else:
-            toExport = str(input("Voulez-vous exporter en CSV ? (o/N) "))
-            if toExport.lower() == 'o':
-                export_as_csv(clean_results)
-            else:
-                print("Merci d'avoir utilisé notre PcapParser !")
-                sys.exit(0)
+            print("Merci d'avoir utilisé notre PcapParser !")
+            sys.exit(0)
 
 def export_as_csv(results):
 
@@ -270,18 +257,12 @@ def export_as_csv(results):
 def getFileName():
     pcap_file = str(input("Veuillez donner le nom du fichier pcap à parser (sans extension) : "))
     pcap_file += ".pcap"
-    
-    user_custom = str(input("Voulez-vous préciser une IP Source / Destination à rechercher ? (o/N) "))
-    if user_custom.lower() == "o":
-        user_ip_src = str(input("IP Source : "))
-        user_ip_dest = str(input("IP Dest : "))
-        if(validate_ip(user_ip_src) and validate_ip(user_ip_dest)):
-            extract_pcap(pcap_file, user_ip_src, user_ip_dest)
-        else:
-            print('Addresses IP non valides !')
-            sys.exit(0)
+
+    if os.path.exists(pcap_file):
+        extract_pcap(pcap_file)
     else:
-        extract_pcap(pcap_file, '', '')
+        print("Le fichier n'existe pas !")
+        sys.exit(0)
 
 def main():
     getFileName()
